4f9f9b232da1f1bb1300b4572a7b8f2e2ab780fe
kaidong <shikaidong@126.com>
2020-01-06 21:39:45 +0800

    add alert

--- bitblack/cmd/test/main.go
+++ bitblack/cmd/test/main.go
@@ -3,24 +3,52 @@ package main
 import (
 	"fmt"
 
+	"sync"
+	"time"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/push"
 )
 
 func pushPrometheus() {
 	completionTime := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "db_backup_last_completion_timestamp_seconds",
+		Name: "koala",
 		Help: "The timestamp of the last successful completion of a DB backup.",
 	})
 	completionTime.SetToCurrentTime()
-	if err := push.New("http://localhost:9091", "db_backup").
+	completionTime.Add(1)
+	if err := push.New("http://localhost:9091", "koala_db_backup").
 		Collector(completionTime).
 		Grouping("db", "customers").
+		Grouping("instance", "koala-1").
 		Push(); err != nil {
 		fmt.Println("Could not push completion time to Pushgateway:", err)
 	}
 }
+
+func timeTicker() {
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	for i := 0; i < 10; i++ {
+		fmt.Println("test")
+		<-ticker.C
+	}
+}
+
+func syncWaitGroup() {
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(index int) {
+			fmt.Println(index)
+			wg.Done()
+		}(i)
+
+	}
+	wg.Wait()
+}
+
 func main() {
-	pushPrometheus()
+	syncWaitGroup()
 	fmt.Println("test")
 }
